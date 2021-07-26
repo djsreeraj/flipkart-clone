05331ba7578b828c2c8c740b63eb29da5e1aa796
djsreeraj <sreeraj011m@gmail.com>
2021-07-26 23:46:27 +0530

    added header logo section

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,57 @@
+import {
+  AppBar,
+  Toolbar,
+  makeStyles,
+  Typography,
+  Box,
+} from "@material-ui/core";
+
+const useStyle = makeStyles({
+  header: {
+    background: "#2874f0",
+    height: 55,
+  },
+  logo: {
+    width: 75,
+  },
+  subURL: {
+    width: 10,
+    marginLeft: 4,
+    height: 10,
+  },
+  container: {
+    display: "flex",
+  },
+  component: {
+    marginLeft: "12%",
+    lineHeight: 0,
+  },
+  subHeading: {
+    fontSize: 10,
+    fontStyle: "italic",
+  },
+});
+
+const Header = () => {
+  const classes = useStyle();
+  const logoURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+  const subURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
+
+  return (
+    <AppBar className={classes.header}>
+      <Toolbar>
+        <Box className={classes.component}>
+          <img src={logoURL} className={classes.logo} />
+          <Box className={classes.container}>
+            <Typography className={classes.subHeading}>Explore Plus</Typography>
+            <img src={subURL} className={classes.subURL} />
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
