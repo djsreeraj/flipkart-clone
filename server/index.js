1b54d9198ed1ffb268a99830f73561515212fce0
djsreeraj <sreeraj011m@gmail.com>
2021-08-11 16:58:41 +0530

    connected mongo db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const PORT = 8000;
+
+const URL = process.env.MONGO_DB_URL;
+
+const connection = async () => {
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+};
+
+connection();
 
 app.listen(PORT, () =>
   console.log(`Server is successfully running on PORT ${PORT}`)
